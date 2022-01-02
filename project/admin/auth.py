f21265266e8d0c59b8f4894a4f7da18dea1b71e1
Eduardo Machado Rezende <eduardomachadorezende0@gmail.com>
2022-01-02 19:36:51 -0300

    add login admin

--- project/admin/auth.py
+++ project/admin/auth.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user
+from models import Admin
 
 auth = Blueprint('auth_admin', __name__, url_prefix='/admin')
 
@@ -10,8 +13,19 @@ def login():
 
 @auth.route('/login', methods=['POST'])
 def login_post():
-    return "aeeeee"
+    email = request.form.get('email')
+    password = request.form.get('password')
+    remember = True if request.form.get('remember') else False
 
+    admin = Admin.query.filter_by(email=email).first()
+
+    if not admin or not check_password_hash(admin.password, password):
+        flash('Please check your login details and try again.')
+        return redirect(url_for('auth_user.login'))
+
+    login_user(admin, remember=remember)
+    return redirect(url_for('main.profile'))
+    
 
 @auth.route('/signup')
 def signup():
